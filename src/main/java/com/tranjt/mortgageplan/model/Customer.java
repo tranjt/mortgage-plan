a92ce1020d7caab47ea73dddb7cc6bfe17b24a29
JTTran <tranjt80@gmail.com>
2021-11-10 22:38:50 +0200

    Added customer and customerRepository

---
+++ src/main/java/com/tranjt/mortgageplan/model/Customer.java
@@ -0,0 +1,93 @@
+package com.tranjt.mortgageplan.model;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double totalLoan;
+
+    @Column(nullable = false)
+    private double interest;
+
+    @Column(nullable = false)
+    private int years;
+
+    public Customer() {
+    }
+
+    public Customer(String name, double totalLoan, double interest, int years) {
+        this.name = name;
+        this.totalLoan = totalLoan;
+        this.interest = interest;
+        this.years = years;
+    }
+
+    public Customer(Long id, String name, double totalLoan, double interest, int years) {
+        this.id = id;
+        this.name = name;
+        this.totalLoan = totalLoan;
+        this.interest = interest;
+        this.years = years;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getTotalLoan() {
+        return totalLoan;
+    }
+
+    public void setTotalLoan(double totalLoan) {
+        this.totalLoan = totalLoan;
+    }
+
+    public double getInterest() {
+        return interest;
+    }
+
+    public void setInterest(double interest) {
+        this.interest = interest;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", totalLoan=" + totalLoan +
+                ", interest=" + interest +
+                ", Years=" + years +
+                '}';
+    }
+}
