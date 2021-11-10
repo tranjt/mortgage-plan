9b2704420776c7ace0284a3854aa9de49fe92ea9
JTTran <tranjt80@gmail.com>
2021-11-10 22:19:18 +0200

    Initial commit

---
+++ src/main/java/com/tranjt/mortgageplan/MortgagePlanApplication.java
@@ -0,0 +1,13 @@
+package com.tranjt.mortgageplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgagePlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgagePlanApplication.class, args);
+	}
+
+}
